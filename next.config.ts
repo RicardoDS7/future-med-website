ef3486331c9caea75732808f60f752a4d64e82af
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-06-23 17:22:18 +0200

    Adjust next config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-// Define your GitHub repo name here
-const repoName = "future-med-website";
-
-// Check if we’re in production (e.g., deployed on GitHub Pages)
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
